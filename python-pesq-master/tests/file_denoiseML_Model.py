8a4d3a2aff0418194ec96d880d741510e0206aba
QuantumNeoChen <QuantumNeoChen@gmail.com>
2022-03-03 05:17:48 +0800

    程式碼修改。改成符合競賽規則(存檔名稱)

--- python-pesq-master/tests/file_denoiseML_Model.py
+++ python-pesq-master/tests/file_denoiseML_Model.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from pesq import pesq
 import scipy.io.wavfile
 
+import os
+
 ##########################
 # the values are fixed, if you need other values, you have to retrain.
 # The sampling rate of 16k is also fix.
@@ -21,54 +23,79 @@ block_len = 512
 block_shift = 128
 
 
-data_dir = Path(__file__).parent.parent / 'audio'
-#ref_path = data_dir / 'speech.wav'
-#deg_path = data_dir / 'speech_bab_0dB.wav'
-#degIN_path = data_dir / 'vocal_01081.flac'
-degSAVE_path = data_dir / 'vocal_01081.wav'
-data_dir = Path(__file__).parent.parent / 'audio'
-ref_path = data_dir / 'mixed_01081_jackhammer.wav'
+# =============================================================================
+# data_dir = Path(__file__).parent.parent / 'audio'
+# #ref_path = data_dir / 'speech.wav'
+# #deg_path = data_dir / 'speech_bab_0dB.wav'
+# #degIN_path = data_dir / 'vocal_01081.flac'
+# degSAVE_path = data_dir / 'vocal_01081.wav'
+# data_dir = Path(__file__).parent.parent / 'audio'
+# ref_path = data_dir / 'mixed_01081_jackhammer.wav'
+# =============================================================================
 
-# load model
-model = tf.saved_model.load('./pretrained_model/dtln_saved_model')
-infer = model.signatures["serving_default"]
-# load audio file at 16k fs (please change)
-audio,fs = sf.read(ref_path)
-# check for sampling rate
-if fs != 16000:
-    raise ValueError('This model only supports 16k sampling rate.')
-# preallocate output audio
-out_file = np.zeros((len(audio)))
-# create buffer
-in_buffer = np.zeros((block_len))
-out_buffer = np.zeros((block_len))
-# calculate number of blocks
-num_blocks = (audio.shape[0] - (block_len-block_shift)) // block_shift
-# iterate over the number of blcoks        
-for idx in range(num_blocks):
-    # shift values and write to buffer
-    in_buffer[:-block_shift] = in_buffer[block_shift:]
-    in_buffer[-block_shift:] = audio[idx*block_shift:(idx*block_shift)+block_shift]
-    # create a batch dimension of one
-    in_block = np.expand_dims(in_buffer, axis=0).astype('float32')
-    # process one block
-    out_block= infer(tf.constant(in_block))['conv1d_1']
-    # shift values and write to buffer
-    out_buffer[:-block_shift] = out_buffer[block_shift:]
-    out_buffer[-block_shift:] = np.zeros((block_shift))
-    out_buffer  += np.squeeze(out_block)
-    # write block to output file
-    out_file[idx*block_shift:(idx*block_shift)+block_shift] = out_buffer[:block_shift]
+data_dir = Path(__file__).parent.parent / 'audio/test2wav'
+data_dir_files = os.listdir(data_dir)
+#data_dir_save = Path(__file__).parent.parent / 'audio/vocal'
+data_dir_saveFlac = Path(__file__).parent.parent / 'audio/vocalFlac'
+
+i=0
+for i_name in data_dir_files:
+    i = i+1
+    # 轉檔 flac to wav #
+    degIN_path = data_dir / i_name 
     
+    # load model
+    model = tf.saved_model.load('./pretrained_model/dtln_saved_model')
+    infer = model.signatures["serving_default"]
+    # load audio file at 16k fs (please change)
     
-refOUT_path = data_dir / 'out.wav'
-# write to .wav file 
-#sf.write('out.wav', out_file, fs)
-sf.write(refOUT_path, out_file, fs)
+    ref_path = data_dir / i_name 
+    
+    audio,fs = sf.read(ref_path)
+    # check for sampling rate
+    if fs != 16000:
+        raise ValueError('This model only supports 16k sampling rate.')
+    # preallocate output audio
+    out_file = np.zeros((len(audio)))
+    # create buffer
+    in_buffer = np.zeros((block_len))
+    out_buffer = np.zeros((block_len))
+    # calculate number of blocks
+    num_blocks = (audio.shape[0] - (block_len-block_shift)) // block_shift
+    # iterate over the number of blcoks        
+    for idx in range(num_blocks):
+        # shift values and write to buffer
+        in_buffer[:-block_shift] = in_buffer[block_shift:]
+        in_buffer[-block_shift:] = audio[idx*block_shift:(idx*block_shift)+block_shift]
+        # create a batch dimension of one
+        in_block = np.expand_dims(in_buffer, axis=0).astype('float32')
+        # process one block
+        out_block= infer(tf.constant(in_block))['conv1d_1']
+        # shift values and write to buffer
+        out_buffer[:-block_shift] = out_buffer[block_shift:]
+        out_buffer[-block_shift:] = np.zeros((block_shift))
+        out_buffer  += np.squeeze(out_block)
+        # write block to output file
+        out_file[idx*block_shift:(idx*block_shift)+block_shift] = out_buffer[:block_shift]
+        
+    f_Name = "vocal_" + str(i).rjust(5,"0") + ".wav"
+    refOUT_path = data_dir_saveFlac / f_Name
+    # write to .wav file 
+    #sf.write('out.wav', out_file, fs)
+    sf.write(refOUT_path, out_file, fs)
+    
+    sample_rate, ref = scipy.io.wavfile.read(refOUT_path)
+    
+    f_Name = "vocal_" + str(i).rjust(5,"0") + ".flac"
+    refOUTflac_path = data_dir_saveFlac / f_Name
+    scipy.io.wavfile.write(refOUTflac_path, sample_rate, ref)
+    
+    print(i_name)
+    print('Processing finished.')
 
-print('Processing finished.')
 
-
-sample_rate1, ref = scipy.io.wavfile.read(degSAVE_path)
-sample_rate2, deg = scipy.io.wavfile.read(refOUT_path)
-score1 = pesq(ref=ref, deg=deg, fs=sample_rate1, mode='wb')
+# =============================================================================
+# sample_rate1, ref = scipy.io.wavfile.read(degSAVE_path)
+# sample_rate2, deg = scipy.io.wavfile.read(refOUT_path)
+# score1 = pesq(ref=ref, deg=deg, fs=sample_rate1, mode='wb')
+# =============================================================================
